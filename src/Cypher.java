a9ffce086c380ab9cde59267486b023c5adb84ec
AlterEgo-Java <whitesafonchikbusiness@gmail.com>
2025-09-25 17:37:19 +0300

    Создал метод encrypt, который шифрует текст.

--- src/Cypher.java
+++ src/Cypher.java
@@ -1,9 +1,8 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Cypher {
 
-    public String userAnswear;
-    private String SystemAnsear;
 
     public static void userInterface() throws InterruptedException {
         boolean appEnd = false;
@@ -12,10 +11,7 @@ public class Cypher {
         Thread.sleep(1000);
         System.out.println("======Класс шифра запущен=====");
 
-        while (appEnd != true) {
-
-
-            Thread.sleep(1500);
+        while (!appEnd) {
             System.out.println("Введите цифру для выбора действия!");
             System.out.println("1. Зашифровать текст 2. Расшифровать текст");
             System.out.println("3. Выйти");
@@ -26,28 +22,38 @@ public class Cypher {
                     System.out.println("Введите текст для шифрования");
                     userAnswear = scanner.nextLine();
                     encrypt(userAnswear);
-                break;
-                    case "2":
-                        System.out.println("error");
-                        break;
+                    break;
+                case "2":
+                    System.out.println("error");
+                    break;
                 case "3":
                     appEnd = true;
                     break;
             }
         }
     }
-    private static String encrypt(String userAnswear) {
+
+    private static void  encrypt(String userAnswear) {
         String[][] keyMass = {
                 {"А", "Б", "В", "Г", "Д", "Е", "Ё", "Ж", "З", "И", "Й", "К", "Л", "М", "Н", "О", "П", "Р", "С", "Т", "У", "Ф", "Х", "Ц", "Ч", "Ш", "Щ", "ъ", "Ы", "Ь", "Э", "Ю", "Я", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".", " ", ",", "!", "?"},
-                {"Г", "Д", "Е", "Ё", "Ж", "З", "И", "Й", "К", "Л", "М", "Н", "О", "П", "Р", "С", "Т", "У", "Ф", "Х", "Ц", "Ч", "Ш", "Щ", "ъ", "Ы", "Ь", "Э", "Ю", "Я", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "А", "Б", "В", "/", "#", "*", "%", "@"}
+                {"Г", "Д", "Е", "Ё", "Ж", "З", "И", "Й", "К", "Л", "М", "Н", "О", "П", "Р", "С", "Т", "У", "Ф", "Х", "Ц", "Ч", "Ш", "Щ", "ъ", "Ы", "Ь", "Э", "Ю", "Я", "А", "Б", "В", "3", "4", "5", "6", "7", "8", "9", "0", "1", "2", "/", "#", "*", "%", "@"}
         };
         String[] massForCrypt = userAnswear.split("");
+        String[] massForUnswear = new String[massForCrypt.length];
+
         for (int i = 0; i < massForCrypt.length; i++) {
-            System.out.println(massForCrypt[i]);
+            for (int x = 0; x < keyMass[0].length; x++) {
+                if (massForCrypt[i].equalsIgnoreCase(keyMass[0][x])) {
+                    char checkletter = massForCrypt[i].charAt(0);
+                    if (Character.isUpperCase(checkletter)) {
+                        massForUnswear[i] = keyMass[1][x];
+                    }else{
+                        massForUnswear[i]= keyMass[1][x].toLowerCase();
+                    }
+
+                }
+            }
         }
-
-
-        return "";
-
+        System.out.println(Arrays.toString(massForUnswear));
     }
 }
